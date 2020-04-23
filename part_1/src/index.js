7fecf6154b53dbe9e799d170f1298d30c12e7824
sumsupee <sumedh_supe@yahoo.in>
2020-04-24 01:04:48 +0530

    1.5 complete

--- part_1/src/index.js
+++ part_1/src/index.js
@@ -38,27 +38,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content part={parts} />
-      <Total exercise={parts} />
+      <Header course={course.name} />
+      <Content part={course.parts} />
+      <Total exercise={course.parts} />
     </div>
   )
 }
